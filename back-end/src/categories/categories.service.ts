bd7212c02e29c38872d934d9fde1e61d72eb6490
ikrammarhaoui <marhaouiikram7@gmail.com>
2026-05-02 12:04:49 +0100

    Categories

---
+++ back-end/src/categories/categories.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@nestjs/common';
+import{PRODUCTS} from '../products/products.data'
+@Injectable()
+export class CategoriesService {
+findAll():string[]{
+const unique=[...new Set(PRODUCTS.map((p)=>p.categorie))];
+return ['Tous',...unique];
+}
+}
