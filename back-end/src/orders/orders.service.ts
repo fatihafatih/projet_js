776c19542494f8144eaaadf65b00e6f6aaae6e6d
fatihafatih <fatihaelhamdani@gmail.com>
2026-05-02 22:44:44 +0000

    update

--- back-end/src/orders/orders.service.ts
+++ back-end/src/orders/orders.service.ts
@@ -1,27 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { readFile, writeFile } from 'fs/promises';
 
 @Injectable()
 export class OrdersService {
-private orders: any[] = [];
-  create(order: any) {
-  const newOrder = {
-    numeroCommande: "CMD-" + Math.floor(Math.random() * 100000),
-    date: new Date().toLocaleDateString("fr-FR"),
-    ...order,
-  };
+  private filePath = 'orders.json';
 
-  this.orders.push(newOrder);
-  return newOrder;
-}
-    
+  async create(order: any) {
+    const orders = await this.findAll();
 
-findAll() {
-  return this.orders;
-}
+    const newOrder = {
+      numeroCommande: "CMD-" + Math.floor(Math.random() * 100000),
+      date: new Date().toLocaleDateString("fr-FR"),
+      ...order,
+    };
 
-findByEmail(email: string) {
-  return this.orders.filter(
-    (order) => order.client.email === email,
-  );
-}
+    orders.push(newOrder);
+
+    await writeFile(this.filePath, JSON.stringify(orders, null, 2));
+
+    return newOrder;
+  }
+
+  async findAll() {
+    try {
+      const data = await readFile(this.filePath, 'utf-8');
+      return JSON.parse(data);
+    } catch {
+      return [];
+    }
+  }
+
+  async findByEmail(email: string) {
+    const orders = await this.findAll();
+    return orders.filter(
+      (order) => order.client.email === email,
+    );
+  }
 }
