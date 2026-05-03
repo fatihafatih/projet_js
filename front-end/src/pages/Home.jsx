8aa8f8cf66929c16be9fdee0a9d9f8989a77f11c
ikrammarhaoui <marhaouiikram7@gmail.com>
2026-05-03 14:57:45 +0100

    images+logo_title

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -12,20 +12,20 @@ function Home({setPage}){
            </div>
             <div className="categories-grid">
                 <div className="btn-two" onClick={()=>setPage("produits")}>
-                  <span className="btn-tree">👕</span>
+                  <span className="btn-tree"><img src="https://image.made-in-china.com/202f0j00gpLRHTArqFkt/Custom-T-Shirts-100-Cotton-Men-Tshirt-Tee-Shirt-Printing-T-Shirt-Polo-T-Shirt-for-Men-Women-Plain-T-Shirt.webp" alt="T-shirt" /></span>
                   <p>T-shirts</p>
                 </div>
                 <div className="btn-two" onClick={()=>setPage("produits")}>
-                  <span className="btn-tree">👖</span>
+                  <span className="btn-tree"><img src="https://img01.ztat.net/article/spp-media-p1/ce700a1bd731458bbc48c32a6e082f6c/8f47ffc12c944a5cb227e4071a180332.jpg?imwidth=200&filter=packshot" alt="Pantalon" /></span>
                   <p>Pantalons</p>
                 </div>
 
                 <div className="btn-two" onClick={()=>setPage("produits")}>
-                  <span className="btn-tree">🧥</span>
+                  <span className="btn-tree"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRluLcs0GfV4-TUWXQll2hcv1s8cU68kymOHg&s"/></span>
                   <p>Vestes</p>
                 </div>
            <div className="btn-two" onClick={()=>setPage("produits")}>
-                  <span className="btn-tree">👗</span>
+                  <span className="btn-tree"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTA6luqDgxk8p5CDuuDGkmZ6gmvCj-2bSxo7A&s"/></span>
                   <p>Robes</p>
                 </div>
              </div>
